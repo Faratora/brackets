3618c7b6847ac351bd95e4a520cf4aceadfcf092
Svetlana <brigantina@mail.ru>
2025-05-12 21:13:18 +0300

    Update index.js
    check task

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   const stack = [];
+  const bracketsMap = new Map(bracketsConfig);
+  const openBrackets = new Set(bracketsConfig.map((pair) => pair[0]));
+  const sameBrackets = new Set(
+    bracketsConfig.filter((pair) => pair[0] === pair[1]).map((pair) => pair[0])
+  );
 
-  function isBrackets(ch) {
-    return [')', '}', ']', '|'].indexOf(ch) > -1;
-  }
-
-  for (let i = 0; i < str.length; i += i) {
+  for (let i = 0; i < str.length; i += 1) {
     const current = str[i];
-    // проверка открывающиеся или закрывающаяся скобка
-    if (isBrackets(current)) {
-      if (bracketsConfig[current] !== stack.pop()) return false;
-    } else {
+
+    if (sameBrackets.has(current)) {
+      // Если скобка одинаковая, проверяем стек
+      if (stack.length && stack[stack.length - 1] === current) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    } else if (openBrackets.has(current)) {
+      // ..Если это открывающая скобка, добавляем её в стек
       stack.push(current);
+    } else {
+      // Если это закрывающая скобка
+      if (stack.length === 0) {
+        return false;
+      }
+      const lastOpened = stack.pop();
+      if (bracketsMap.get(lastOpened) !== current) {
+        return false;
+      }
     }
   }
 
